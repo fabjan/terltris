f7bdec0ed3e50e6678a369e42abb59388e6535c9
Fabian Bergstrom <sempetmer@gmail.com>
2008-11-16 00:00:58 +0100

    Change from opengl to sdl for drawing

--- src/gui.erl
+++ src/gui.erl
@@ -9,71 +9,42 @@
 
 -compile(export_all).
 
--record(state, {width, height, game, delay0 = 10, delay = 0}).
+-record(state, {window, game, sprites, delay0 = 10, delay = 0}).
 
-init() ->
-    Width  = 400,
-    Height = 800,
-    Vst = init_video(Width, Height),
-    if
-        Vst == error ->
+-define(WIDTH, 400).
+-define(HEIGHT, 800).
+-define(BPP, 24).
+
+init(Width, Height) ->
+    case init_video() of
+        error ->
             sdl:quit(),
             error;
-        true ->
+        Window ->
             sdl_video:wm_setCaption("terltris", 0),
-            init_gl(),
             {A, B, C} = now(), random:seed(A, B, C),
-            Game = game:new(10, 20),
-            loop(#state{width = Width, height = Height, game = Game}),
+            Game = game:new(Width, Height),
+            Sprites = load_sprites([g,i,j,l,o,s,t,z]),
+            loop(#state{window = Window, game = Game, sprites = Sprites}),
             sdl:quit()
     end.
 
-init_video(W, H) ->
-    sdl:init(?SDL_INIT_VIDEO bor
-             ?SDL_INIT_ERLDRIVER bor
-             ?SDL_INIT_NOPARACHUTE),
-    Bpp = 16,
-    Surface = sdl_video:setVideoMode(W, H, Bpp, 
-                                     ?SDL_OPENGL bor
-                                     ?SDL_RESIZABLE bor 
-                                     ?SDL_GL_DOUBLEBUFFER),   
-    if 
-        Surface == error ->
-            io:format("Can't set video mode~n", []),
-            error;
-        true ->
-            sdl_video:gl_setAttribute(?SDL_GL_DOUBLEBUFFER, 1),
-            resize_window(W, H)
-    end.
-
-init_gl() ->
-    gl:shadeModel(?GL_SMOOTH),
-    gl:clearColor(0.0, 0.0, 0.0, 0.0),
-    gl:clearDepth(1.0),
-    gl:enable(?GL_DEPTH_TEST),
-    gl:depthFunc(?GL_LEQUAL),
-    gl:hint(?GL_PERSPECTIVE_CORRECTION_HINT, ?GL_NICEST).
+init_video() ->
+    sdl:init(?SDL_INIT_VIDEO),
+    resize_window(?WIDTH, ?HEIGHT).
 
 resize_window(Width, Height) ->
-    Bpp = 16,
-    Surface = sdl_video:setVideoMode(Width, Height, Bpp, 
-                                     ?SDL_OPENGL bor ?SDL_RESIZABLE bor 
-                                     ?SDL_GL_DOUBLEBUFFER),   
-    if 
-        Surface == error ->
+    case sdl_video:setVideoMode(Width, Height, ?BPP,
+                                ?SDL_HWSURFACE bor
+                                ?SDL_RESIZABLE bor
+                                ?SDL_DOUBLEBUF) of
+        error ->
             io:format("Can't set video mode~n", []),
             error;
-       true ->
+        Surface ->
             sdl_video:gl_setAttribute(?SDL_GL_DOUBLEBUFFER, 1),
-            Ratio = Width / Height,    
-            gl:viewport(0, 0, Width, Height),
-            gl:matrixMode(?GL_PROJECTION),
-            gl:loadIdentity(),
-            glu:perspective(45.0, Ratio, 0.1, 100.0),
-            gl:matrixMode(?GL_MODELVIEW),
-            gl:loadIdentity()
+            Surface
     end.
-
 
 loop(State) ->
     render(State),
@@ -87,50 +58,29 @@ loop(State) ->
             loop(NewState#state{delay = Delay - 1})
     end.
 
-render(#state{game = Game, width = Width, height = Height}) ->
-    gl:clear(?GL_COLOR_BUFFER_BIT bor ?GL_DEPTH_BUFFER_BIT),
-    Origin = origin(Width, Height),
-    lists:foreach(fun (Block) -> draw_block(Block, Origin) end,
+render(#state{window = Window, game = Game, sprites = Sprites}) ->
+    sdl_video:fillRect(Window, null, sdl_video:mapRGB(Window, 0, 0, 0)),
+    lists:foreach(fun (Block) -> draw_block(Block, Window, Sprites) end,
                   game:blocks(Game)),
-    T = gl:swapBuffers().
+    sdl_video:flip(Window).
 
 origin(Width, Height) ->
     Ratio = Width/Height,
     Scale = 40,
     {-Width/Scale, Height/(Scale/Ratio), -32}.
 
-draw_block(Block = {{X, Y}, Shape}, {X0, Y0, Z0}) ->
-    {R, G, B} = color(Shape),
-    Outline   = 0.5,
-    Fill      = Outline * 0.9,
-    gl:loadIdentity(),
-    gl:translatef(X0+X, Y0+Y, Z0),
-    gl:color3f(R-0.2, G-0.2, B-0.2),
-    gl:glBegin(?GL_QUADS),
-    gl:vertex3f(-Outline, Outline, 0.0),
-    gl:vertex3f(Outline, Outline, 0.0),
-    gl:vertex3f(Outline, -Outline, 0.0),
-    gl:vertex3f(-Outline, -Outline, 0.0),
-    gl:glEnd(),
-    gl:color3f(R, G, B),
-    gl:glBegin(?GL_QUADS),
-    gl:vertex3f(-Fill, Fill, 0.0),
-    gl:vertex3f(Fill, Fill, 0.0),
-    gl:vertex3f(Fill, -Fill, 0.0),
-    gl:vertex3f(-Fill, -Fill, 0.0),
-    gl:glEnd().
-
-color(i) -> {0.0, 1.0, 1.0};
-color(j) -> {0.0, 0.0, 1.0};
-color(l) -> {1.0, 0.5, 0.0};
-color(o) -> {1.0, 1.0, 0.0};
-color(s) -> {0.0, 1.0, 0.0};
-color(t) -> {1.0, 0.0, 1.0};
-color(z) -> {1.0, 0.0, 0.0};
-color(g) -> {0.5, 0.5, 0.5}.
+draw_block(_Block = {{X, Y}, Shape}, Window, Sprites) ->
+    {value, {_Shape, Sprite}} = lists:keysearch(Shape, 1, Sprites),
+    Source = #sdl_rect{x = 0, y = 0, w = 32, h = 32},
+    #sdl_surface{w = WWidth, h = WHeight} = sdl_video:getSurface(Window),
+    %%Width = WWidth/10, Height = WHeight/20,
+    Width = Height = 32,
+    Dest = #sdl_rect{x = round(X*Width), y = round(-Y*Width),
+                     w = round(Width), h = round(Height)},
+    sdl_video:blitSurface(Sprite, Source, Window, Dest).
 
 load_sprites(L) ->
-    [{Shape, sdl_video:loadBMP("sprites/" ++ atom_to_list(Shape) ++ ".bmp")}
+    [{Shape, sdl_video:loadBMP("src/sprites/" ++ atom_to_list(Shape) ++ ".bmp")}
      || Shape <- L].
 
 handle_event(State = #state{game = Game}) ->
